15551b39cdd92187e149f83a0b7436fd1a14b701
Alex Bazlinton <abazlinton@gmail.com>
2016-12-04 21:46:16 +0000

    MVP + two extentions tests pass

--- record_store.js
+++ record_store.js
@@ -1,14 +1,40 @@
+var pry = require("pryjs");
+
 var RecordStore = function(params){
   this.name = params.name;
   this.city = params.city;
   this.records = params.records;
-  
+  this.balance = params.balance;
 };
 
 RecordStore.prototype = {
   addRecord: function(record){
     this.records.push(record);
+  },
+
+  listInventory: function(){
+    var outputString = ""
+    this.records.forEach(function(record){
+      outputString += record.title + ", by " + record.artist + ". Price: £" + record.price.toString() + "\n";
+    });
+    return outputString;
+  },
+
+  sellRecord: function(recordNumber){
+    this.balance += this.records[recordNumber].price;
+    return this.records.splice(recordNumber, 1)[0];
+  },
+
+  reportFinances: function(){
+    var valueOfStock = 0;
+    var totalAssets = 0;
+    this.records.forEach(function(record){
+      valueOfStock += record.price;
+    });
+    totalAssets = this.balance + valueOfStock;
+    return "Cash: " + this.balance + ", Stock value: £" + valueOfStock + ". Total assets: £" + totalAssets;
   }
+
 };
 
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -10,9 +10,8 @@ describe("RecordStore", function(){
     var recordsStub = [];
     var record1 = {title: "II", artist: "Moderat", price: 9.99};
     var record2 = {title: "Revolver", artist: "The Beatles", price: 13.99};
-    recordsStub.push(record1);
-    recordsStub.push(record2);
-    recordStore = new RecordStore({name: "Rollin' Rick's", city: "Boston", records: recordsStub});
+    recordsStub = [record1, record2];
+    recordStore = new RecordStore({name: "Rollin' Rick's", city: "Boston", records: recordsStub, balance: 1000});
   });
 
   it("has a name", function(){
@@ -31,6 +30,29 @@ describe("RecordStore", function(){
     var recordStub = {title: "II", artist: "Moderat", price: 9.99}
     recordStore.addRecord(recordStub);
     assert.equal("II", recordStore.records[2].title);
+  });
+
+  it("has a balance", function(){
+    assert.equal(1000, recordStore.balance);
+  });
+
+  it("can list inventory", function(){
+    var output = recordStore.listInventory();
+    assert.equal("II, by Moderat. Price: £9.99\nRevolver, by The Beatles. Price: £13.99\n", output);
+  });
+
+  it("can sell a record", function(){
+    var soldRecord = recordStore.sellRecord(0);
+    assert.equal("Moderat", soldRecord.artist);
+    assert.equal("II", soldRecord.title);
+    assert.equal(1009.99, recordStore.balance);
+    assert.equal(13.99, recordStore.records[0].price);
+  });
+
+  it("can report finanical situation", function(){
+    var output = recordStore.reportFinances();
+    assert.equal("Cash: 1000, Stock value: £23.98. Total assets: £1023.98", output);  
+
   });
 
 
